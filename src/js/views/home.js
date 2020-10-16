2800745cd448caa97dcf7e40bcd605162b75fdf9
vcompanyb <vcompanyb@vulturdev.com>
2020-10-16 18:11:06 +0000

    Change

--- src/js/views/home.js
+++ src/js/views/home.js
@@ -9,7 +9,7 @@ export const Home = () => (
 			<img src={rigoImage} />
 		</p>
 		<a href="#" className="btn btn-success">
-			If you see this green button
+			If you see this green button, bootstrap is working
 		</a>
 	</div>
 );
